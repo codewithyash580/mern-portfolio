9e616109eaf0c5bd762da497abe18bc4da8a8e9f
yashchauhan2025 <chauhanyash098234@gmail.com>
2026-08-07 09:43:35 +0530

    fix server connection error

--- server/server.js
+++ server/server.js
@@ -5,20 +5,26 @@ import connectDB from "./config/db.js";
 import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
+
 app.get('/health',(req,res) =>{
-    res.status(200).send('ok')
-})
+    res.status(200).send('ok');
+});
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/contact", contactRoutes);
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() =>{
-    console.log('server is running on port ${PORT}')
-})
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed:", err);
+  });
